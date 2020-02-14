5c17ea86c147a8c1765437bc0424ecbb46ce4f52
Thieres Lima <thieresllima@gmail.com>
2020-02-14 14:36:32 -0300

    Rendering  JSX Elements (Aux or React.Fragment)

--- Diving Deeper intoComponents and React Internals/src/components/Persons/Person/Person.js
+++ Diving Deeper intoComponents and React Internals/src/components/Persons/Person/Person.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react'
 import styles from './Person.module.css'
 
+import Auxiliary from '../../../hoc/Auxiliary/Auxiliary';
+
 class Person extends Component {
     render() {
         console.log('[Person.js] rendering...');
-        return (
-            <div className={styles.Person}>
+        return (    // React.Fragment pode ser uma alternativa para o Aux.
+            <Auxiliary>
                 <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.changed} value={this.props.name}></input>
-            </div>
-        );
+            </Auxiliary>
+        )
     }
 };
 
