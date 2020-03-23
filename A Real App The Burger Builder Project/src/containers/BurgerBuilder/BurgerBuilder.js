d98d1c021e76ac56d3d75944c90d32cd41104d53
Thieres Lima <thieresllima@gmail.com>
2020-03-23 16:40:29 -0300

    Navigating to the Checkout Page

--- A Real App The Burger Builder Project/src/containers/BurgerBuilder/BurgerBuilder.js
+++ A Real App The Burger Builder Project/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -27,6 +27,7 @@ class BurgerBuilder extends Component {
     }
 
     componentDidMount() {
+        console.log(this.props)
         axios.get('https://react-my-burger-dd3f5.firebaseio.com/ingredients.json')
             .then(response => {
                 this.setState({ ingredients: response.data })
@@ -90,28 +91,29 @@ class BurgerBuilder extends Component {
 
     purchaseContiueHandler = () => {
         // alert('You continue!');
-        this.setState({ loading: true })
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.totalPrice,
-            customer: {
-                name: 'Thieres Lima',
-                address: {
-                    streat: 'TestStreet',
-                    zipCode: '12345',
-                    country: 'Brazil'
-                },
-                email: 'test@email.com'
-            },
-            deliveryMethod: 'fastest'
-        }
-        axios.post('/orders.json', order)
-            .then(response => {
-                this.setState({ loading: false, purchasing: false });
-            })
-            .catch(error => {
-                this.setState({ loading: false, purchasing: false });
-            });
+        // this.setState({ loading: true })
+        // const order = {
+        //     ingredients: this.state.ingredients,
+        //     price: this.totalPrice,
+        //     customer: {
+        //         name: 'Thieres Lima',
+        //         address: {
+        //             streat: 'TestStreet',
+        //             zipCode: '12345',
+        //             country: 'Brazil'
+        //         },
+        //         email: 'test@email.com'
+        //     },
+        //     deliveryMethod: 'fastest'
+        // }
+        // axios.post('/orders.json', order)
+        //     .then(response => {
+        //         this.setState({ loading: false, purchasing: false });
+        //     })
+        //     .catch(error => {
+        //         this.setState({ loading: false, purchasing: false });
+        //     });
+        this.props.history.push("/checkout")
     }
 
     render() {
