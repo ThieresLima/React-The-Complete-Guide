b692acca231be5ae19c207ef17cc90244514e8f0
Thieres Lima <thieresllima@gmail.com>
2020-03-25 11:31:55 -0300

    Order Submission & Passing Data Between Pages

--- A Real App The Burger Builder Project/src/containers/BurgerBuilder/BurgerBuilder.js
+++ A Real App The Burger Builder Project/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -91,32 +91,12 @@ class BurgerBuilder extends Component {
 
     purchaseContiueHandler = () => {
         // alert('You continue!');
-        // this.setState({ loading: true })
-        // const order = {
-        //     ingredients: this.state.ingredients,
-        //     price: this.totalPrice,
-        //     customer: {
-        //         name: 'Thieres Lima',
-        //         address: {
-        //             streat: 'TestStreet',
-        //             zipCode: '12345',
-        //             country: 'Brazil'
-        //         },
-        //         email: 'test@email.com'
-        //     },
-        //     deliveryMethod: 'fastest'
-        // }
-        // axios.post('/orders.json', order)
-        //     .then(response => {
-        //         this.setState({ loading: false, purchasing: false });
-        //     })
-        //     .catch(error => {
-        //         this.setState({ loading: false, purchasing: false });
-        //     });
+
         const queryParams = [];
         for (let i in this.state.ingredients) {
             queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
         }
+        queryParams.push('price=' + this.state.totalPrice);
         const queryString = queryParams.join('&');
         this.props.history.push({
             pathname: '/checkout',
