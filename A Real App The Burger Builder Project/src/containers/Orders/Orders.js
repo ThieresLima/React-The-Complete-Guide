a3990cd896313d1577766d6768be43dec4807025
Thieres Lima <thieresllima@gmail.com>
2020-03-26 16:12:09 -0300

    Outputting the Orders

--- A Real App The Burger Builder Project/src/containers/Orders/Orders.js
+++ A Real App The Burger Builder Project/src/containers/Orders/Orders.js
@@ -11,7 +11,7 @@ class Orders extends Component {
     }
 
     componentDidMount() {
-        axios.get('/orders')
+        axios.get('/orders.json')
             .then(res => {
                 const fetchedOrders = [];
                 for (let key in res.data) {
@@ -30,8 +30,12 @@ class Orders extends Component {
     render() {
         return (
             <div>
-                <Order />
-                <Order />
+                {this.state.orders.map(order => (
+                    <Order
+                        key={order.id}
+                        ingredients={order.ingredients}
+                        price={order.price} />
+                ))}
             </div>
         );
     }
