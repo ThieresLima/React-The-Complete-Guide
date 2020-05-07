2a8a265fb38233434e538093ce4fcbfbb20f81c4
Thieres Lima <thieresllima@gmail.com>
2020-05-07 10:46:37 -0300

    Adding Sign-In

--- A Real App The Burger Builder Project/src/store/actions/auth.js
+++ A Real App The Burger Builder Project/src/store/actions/auth.js
@@ -22,7 +22,7 @@ export const authFail = error => {
     };
 };
 
-export const auth = (email, password) => {
+export const auth = (email, password, isSignup) => {
     return dispatch => {
         dispatch(authStart());
         const authData = {
@@ -30,7 +30,12 @@ export const auth = (email, password) => {
             password: password,
             returnSecureToken: true
         };
-        axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAgyLKimQQ41_BrbTSijWrhFfyvlVdZxPY', authData)
+
+        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAgyLKimQQ41_BrbTSijWrhFfyvlVdZxPY';
+        if (!isSignup) {
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAgyLKimQQ41_BrbTSijWrhFfyvlVdZxPY';
+        }
+        axios.post(url, authData)
             .then(response => {
                 console.log(response);
                 dispatch(authSuccess(response.data));
